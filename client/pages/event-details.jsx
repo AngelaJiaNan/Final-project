7b87c6202a02bf2065d08b9c6984cf34fb6417d3
Angela Jia Nan <angelanan12@gmail.com>
2022-01-06 04:38:30 +0000

    Css finished for event-detail page.

--- client/pages/event-details.jsx
+++ client/pages/event-details.jsx
@@ -10,7 +10,6 @@ export default class EventDetails extends React.Component {
   }
 
   componentDidMount() {
-    console.log('fetch');
     fetch(`/api/events/${this.props.eventID}`)
       .then(res => res.json())
       .then(data => this.setState({ event: data }));
@@ -26,35 +25,36 @@ export default class EventDetails extends React.Component {
     const mapCoordinates = { lat: modlat, lng: modlng };
     const modDate = date.split('T')[0];
     const modAddress = address.split('+').join(' ');
+    const modCity = city.split('+').join(' ');
+    const cityState = `${modCity}, ${state}`;
 
     return (
       <div className='eventdetail-container'>
-        <div className='nav-back'>
-          <a href="#">
-            <i className="fas fa-angle-double-left"></i>
-              <h1>Take Me back</h1>
+          <a className='backbtn' href="#">
+          &lt; Back to event
           </a>
-        </div>
         <div className='event-infor'>
           <div className='event-title'>
             <h2>{title}</h2>
           </div>
           <div className='event-content'>
-            <p>Date:{modDate}</p>
-            <p>Starting Time:{startingtime}</p>
-            <p>Address:{modAddress}</p>
-            <p>City:{city}</p>
+            <p>Date: {modDate}</p>
+            <p>Time: {startingtime}</p>
+            <p>Address: {modAddress}</p>
+            <p>{cityState}</p>
           </div>
-        </div>
-        <div style={{ height: '50vh', width: '50%', padding: '20px' }}>
+          <div style={{ height: '40vh', width: '40%', padding: '10px', margin: 'auto' }}>
           <GoogleMapReact
             bootstrapURLKeys={{ key: 'AIzaSyATRROv2KEQF0wX2e5OPR1CCbNaWFgrpcA' }}
             center={mapCoordinates}
             defaultZoom={10}
           >
           </GoogleMapReact>
+          </div>
+          </div>
+          <div className='submit-btn'>
+        <button id='edit-btn'>Edit Events</button>
         </div>
-        <button>Edit Events</button>
       </div>
     );
   }
