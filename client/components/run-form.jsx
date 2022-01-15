698de2d79a0a2d0e261c95c4bff2eb59ae5c4bc2
Angela Jia Nan <angelanan12@gmail.com>
2022-01-15 00:31:37 +0000

    finished handleSubmit.

--- client/components/run-form.jsx
+++ client/components/run-form.jsx
@@ -12,6 +12,7 @@ export default class RunForm extends React.Component {
     };
     this.handleSumbit = this.handleSumbit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleDate = this.handleDate.bind(this);
   }
 
   handleChange(event) {
@@ -20,15 +21,26 @@ export default class RunForm extends React.Component {
     this.setState({ [name]: value });
   }
 
+  handleDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
   handleSumbit() {
     event.preventDefault();
   }
 
   render() {
+    // console.log('STATES:', this.state);
     return (
     <>
+    <div>
       <div>
-        <form>
+        <h1>New Run</h1>
+        </div>
+        <div>
+            <form onSubmit={this.handleSubmit}>
           <div>
             <label>Date</label>
               <DatePicker selected={this.state.date}
@@ -39,34 +51,26 @@ export default class RunForm extends React.Component {
             <label>Duration</label>
             <input
             name="duration"
-            type="number"
+            type="text"
             id="duration"
-            value=""
-            onChange="" />
+            value={this.state.duration}
+            onChange={this.handleChange} />
           </div>
           <div>
             <label>Distance</label>
             <input
             name="distance"
-            type="number"
+            type="text"
             id="distance"
-            value=""
-            onChange="" />
+            value={this.state.distance}
+            onChange={this.handleChange} />
           </div>
-            <div className='form-inputs'>
-                <label>Starting Time</label>
-                <input
-                  name="startingtime"
-                  type="time"
-                  id="startingTime"
-                  value={this.state.startingtime}
-                  onChange={this.handleChange} />
-              </div>
             <div>
               <button type='submit'>Add Run</button>
             </div>
         </form>
       </div>
+    </div>
     </>
     );
   }
