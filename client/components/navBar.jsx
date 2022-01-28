1f3139c4f8801608e9cdd95f9ab1b90bd981ed95
Angela Jia Nan <angelanan12@gmail.com>
2022-01-28 20:30:31 +0000

    fixed navBar.

--- client/components/navBar.jsx
+++ client/components/navBar.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
 
 export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({ isOpen: false });
+    this.handleMenuContent = this.handleMenuContent.bind(this);
+    this.handleMenuOpen = this.handleMenuOpen.bind(this);
+    this.handleMenuclose = this.handleMenuclose.bind(this);
+  }
+
+  handleMenuOpen() {
+    this.setState({ isOpen: true });
+  }
+
+  handleMenuContent() {
+    this.setState({ isOpen: false });
+  }
+
+  handleMenuclose() {
+    return (this.state.isOpen ? '' : 'hidden');
+  }
+
   render() {
+    const hidden = this.handleMenuclose();
     return (
-      <>
-      <nav className='header'>
-        <div className="row">
-          <div className='col-logo'>
-              <a className='nav-logo' href='#'>Run With Me</a>
-              <i className='fas fa-running' id='running-man'></i>
-          </div>
-          <div className='col-navitem'>
-            <a className='nav-items' href= '#runs'>Runs</a>
-              <a className='nav-items'href='#create'>Create Events</a>
-              <a className='nav-items' href='#signup'>Sign In</a>
-          </div>
+      <div className='row'>
+        <div onClick={this.handleMenuOpen}className='icon'>
+          <i className='fas fa-running'></i>
         </div>
-      </nav>
-      </>
+        <div className='header'>
+          <h1>Run With Me</h1>
+        </div>
+        <div onClick={this.handleMenuContent}className={`container ${hidden}`}>
+          <nav className="menu-content">
+            <a onClick={this.handleMenuContent} href='#'>Home</a>
+            <a onClick={this.handleMenuContent} href= '#runs'>Runs</a>
+            <a onClick={this.handleMenuContent} href='#create'>Create Events</a>
+            <a onClick={this.handleMenuContent} href='#signup'>Sign In</a>
+          </nav>
+        </div>
+      </div>
     );
   }
 }
