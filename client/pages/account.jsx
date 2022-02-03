0edc0137995584d0f01d6ddb99dd5c8fada5807e
Angela Jia Nan <angelanan12@gmail.com>
2022-02-03 00:33:14 +0000

    fixed backend

--- client/pages/account.jsx
+++ client/pages/account.jsx
@@ -37,7 +37,7 @@ export default class Account extends React.Component {
           window.location.hash = '#';
         } else if (action === 'sign-in') {
           window.localStorage.setItem('token', result.token);
-          window.location.hash = '#runs';
+          window.location.hash = '#';
         }
       })
       .catch(err => console.error(err));
